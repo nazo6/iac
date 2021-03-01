e6ec37f318c44ffe77c5b74a89e80cd2dd9f8a93
nazo6 <53177992+nazo6@users.noreply.github.com>
2021-03-01 13:58:17 +0900

    Added production build.

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const common = require('./webpack.common');
 
 const path = require('path');
 
-const appDirectory = path.resolve(__dirname, '../../');
+const appDirectory = path.resolve(__dirname, './');
 
 module.exports = merge(common, {
   mode: 'production',
