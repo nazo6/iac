224b6a41c70232549a4bff15f478fdef1758ac2f
nazo6 <53177992+x-nazo@users.noreply.github.com>
2021-03-07 11:54:34 +0900

    Changed to vite.

--- src/pages/MainPage/components/Main/index.tsx
+++ src/pages/MainPage/components/Main/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-import { Box, Container } from '@material-ui/core';
+import { Box } from '@material-ui/core';
 import { Route, Switch, useHistory, useRouteMatch } from 'react-router-dom';
 
 import Explorer from './Explorer';
