b7bec3545fe5847ae664243c5c38bd19eaa78a19
nazo6 <53177992+x-nazo@users.noreply.github.com>
2021-03-13 17:45:51 +0900

    Use vite-tsconfig-paths.

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,8 @@
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import path from 'path';
 import { defineConfig } from 'vite';
 import WindiCSS from 'vite-plugin-windicss';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), WindiCSS()],
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, './src'),
-    },
-  },
+  plugins: [reactRefresh(), WindiCSS(), tsconfigPaths()],
 });
